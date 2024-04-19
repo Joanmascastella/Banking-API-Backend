619174d4b0b82e53bc5152f7c30f0d64af2d6450
joanasdasd <33759606+Joanmascastella@users.noreply.github.com>
2024-04-19 12:20:32 +0200

    Creating Boilerplate struture

---
+++ src/test/java/com/BankingAPI/BankingAPI/Group1/BankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BankingAPI.BankingAPI.Group1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
