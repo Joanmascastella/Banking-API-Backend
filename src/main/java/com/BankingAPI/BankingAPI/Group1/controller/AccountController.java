da02510bb64f59e35e8ee3d08f71979469594be2
joanasdasd <33759606+Joanmascastella@users.noreply.github.com>
2024-04-26 11:47:08 +0200

    JWT Tokens being generated and filtered with a WebSecurityConfig implemented

--- src/main/java/com/BankingAPI/BankingAPI/Group1/controller/AccountController.java
+++ src/main/java/com/BankingAPI/BankingAPI/Group1/controller/AccountController.java
@@ -2,6 +2,7 @@ package com.BankingAPI.BankingAPI.Group1.controller;
 
 import com.BankingAPI.BankingAPI.Group1.service.AccountService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class AccountController {
 
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_EMPLOYEE')")
     public ResponseEntity<Object> getAllAccounts() {
         return ResponseEntity.status(200).body(accountService.getAllAccounts());
     }
