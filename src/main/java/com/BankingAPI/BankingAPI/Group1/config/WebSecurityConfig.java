c0c20c51a3cf41d5991e1f4ec4e0f77f62439f16
joanasdasd <33759606+Joanmascastella@users.noreply.github.com>
2024-05-21 12:02:31 +0200

    allowing hostend frontend to be in cors policy

--- src/main/java/com/BankingAPI/BankingAPI/Group1/config/WebSecurityConfig.java
+++ src/main/java/com/BankingAPI/BankingAPI/Group1/config/WebSecurityConfig.java
@@ -34,7 +34,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://bankingapinh.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
